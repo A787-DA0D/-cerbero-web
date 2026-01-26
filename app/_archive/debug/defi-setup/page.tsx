3e9c5be1e353ce28d85a7dc4c549992f983f4888
Michele Antonucci <info@cerberoai.com>
2026-01-26 16:07:31 +0100

    chore: remove legacy _archive

--- app/_archive/debug/defi-setup/page.tsx
+++
@@ -1,8 +0,0 @@
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-import Client from "./Client";
-
-export default function Page() {
-  return <Client />;
-}
--- app/_archive/debug/withdraw-usdc/page.tsx
+++
@@ -1,8 +0,0 @@
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-import WithdrawUsdcClient from "./Client";
-
-export default function Page() {
-  return <WithdrawUsdcClient />;
-}
